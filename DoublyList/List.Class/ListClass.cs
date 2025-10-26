9c66700f0ba3cd31c21700d394bcb80d34033475
Herrera <sherreraalzate2@gmail.com>
2025-10-26 07:48:33 -0500

    Finish the work

---
+++ DoublyList/List.Class/ListClass.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List.Class
+{   
+        public class Node<T> where T : IComparable<T>
+        {
+            public T Data;
+            public Node<T> Next;
+            public Node<T> Prev;
+
+            public Node(T data)
+            {
+                Data = data;
+                Next = null;
+                Prev = null;
+            }
+        }
+
+        public class DoublyLinkedList<T> where T : IComparable<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+
+            public void AddOrdered(T data)
+            {
+                Node<T> newNode = new Node<T>(data);
+
+                if (head == null)
+                {
+                    head = tail = newNode;
+                }
+                else if (data.CompareTo(head.Data) <= 0)
+                {
+                    newNode.Next = head;
+                    head.Prev = newNode;
+                    head = newNode;
+                }
+                else if (data.CompareTo(tail.Data) >= 0)
+                {
+                    tail.Next = newNode;
+                    newNode.Prev = tail;
+                    tail = newNode;
+                }
+                else
+                {
+                    Node<T> aux = head;
+                    while (aux.Next != null && aux.Next.Data.CompareTo(data) < 0)
+                    {
+                        aux = aux.Next;
+                    }
+                    newNode.Next = aux.Next;
+                    newNode.Prev = aux;
+                    aux.Next.Prev = newNode;
+                    aux.Next = newNode;
+                }
+            }
+
+            public void DisplayForward()
+            {
+                Node<T> aux = head;
+                while (aux != null)
+                {
+                    Console.Write(aux.Data + " ");
+                    aux = aux.Next;
+                }
+                Console.WriteLine();
+            }
+
+            public void DisplayBackward()
+            {
+                Node<T> aux = tail;
+                while (aux != null)
+                {
+                    Console.Write(aux.Data + " ");
+                    aux = aux.Prev;
+                }
+                Console.WriteLine();
+            }
+
+            public void SortDescending()
+            {
+                if (head == null) return;
+                Node<T> aux = head;
+                while (aux != null)
+                {
+                    Node<T> temp = aux.Next;
+                    while (temp != null)
+                    {
+                        if (aux.Data.CompareTo(temp.Data) < 0)
+                        {
+                            T swap = aux.Data;
+                            aux.Data = temp.Data;
+                            temp.Data = swap;
+                        }
+                        temp = temp.Next;
+                    }
+                    aux = aux.Next;
+                }
+            }
+
+            public void Moda()
+            {
+                if (head == null)
+                {
+                    Console.WriteLine("Lista vacía.");
+                    return;
+                }
+
+                Dictionary<T, int> freq = new Dictionary<T, int>();
+                Node<T> aux = head;
+                int max = 0;
+
+                while (aux != null)
+                {
+                    if (!freq.ContainsKey(aux.Data))
+                        freq[aux.Data] = 0;
+
+                    freq[aux.Data]++;
+                    if (freq[aux.Data] > max)
+                        max = freq[aux.Data];
+
+                    aux = aux.Next;
+                }
+
+                Console.Write("Moda(s): ");
+                foreach (var item in freq)
+                {
+                    if (item.Value == max)
+                        Console.Write(item.Key + " ");
+                }
+                Console.WriteLine();
+            }
+
+            public bool Exists(T data)
+            {
+                Node<T> aux = head;
+                while (aux != null)
+                {
+                    if (aux.Data.CompareTo(data) == 0)
+                        return true;
+                    aux = aux.Next;
+                }
+                return false;
+            }
+
+            public void RemoveOne(T data)
+            {
+                Node<T> aux = head;
+                while (aux != null)
+                {
+                    if (aux.Data.CompareTo(data) == 0)
+                    {
+                        if (aux == head)
+                        {
+                            head = head.Next;
+                            if (head != null) head.Prev = null;
+                        }
+                        else if (aux == tail)
+                        {
+                            tail = tail.Prev;
+                            tail.Next = null;
+                        }
+                        else
+                        {
+                            aux.Prev.Next = aux.Next;
+                            aux.Next.Prev = aux.Prev;
+                        }
+                        Console.WriteLine($"Se eliminó una ocurrencia de {data}");
+                        return;
+                    }
+                    aux = aux.Next;
+                }
+                Console.WriteLine("No se encontró el dato.");
+            }
+
+            public void RemoveAll(T data)
+            {
+                bool removed = false;
+                Node<T> aux = head;
+
+                while (aux != null)
+                {
+                    if (aux.Data.CompareTo(data) == 0)
+                    {
+                        removed = true;
+                        if (aux == head)
+                        {
+                            head = head.Next;
+                            if (head != null) head.Prev = null;
+                        }
+                        else if (aux == tail)
+                        {
+                            tail = tail.Prev;
+                            if (tail != null) tail.Next = null;
+                        }
+                        else
+                        {
+                            aux.Prev.Next = aux.Next;
+                            aux.Next.Prev = aux.Prev;
+                        }
+                    }
+                    aux = aux.Next;
+                }
+
+                Console.WriteLine(removed
+                    ? $"Se eliminaron todas las ocurrencias de {data}"
+                    : "No se encontró el dato.");
+            }
+
+            public void ShowGraph()
+            {
+                if (head == null)
+                {
+                    Console.WriteLine("Lista vacía.");
+                    return;
+                }
+
+                Dictionary<T, int> freq = new Dictionary<T, int>();
+                Node<T> aux = head;
+                while (aux != null)
+                {
+                    if (!freq.ContainsKey(aux.Data))
+                        freq[aux.Data] = 0;
+
+                    freq[aux.Data]++;
+                    aux = aux.Next;
+                }
+
+                foreach (var item in freq)
+                {
+                    Console.Write(item.Key + " ");
+                    for (int i = 0; i < item.Value; i++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+
+
---
+++ DoublyList/List.Console/ListConsole.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using List.Class;
+
+namespace ListConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList<string> list = new DoublyLinkedList<string>();
+            int option;
+
+            do
+            {
+                Console.WriteLine("\n--- MENU ---");
+                Console.WriteLine("1. Adicionar");
+                Console.WriteLine("2. Mostrar hacia adelante");
+                Console.WriteLine("3. Mostrar hacia atrás");
+                Console.WriteLine("4. Ordenar descendentemente");
+                Console.WriteLine("5. Mostrar la(s) moda(s)");
+                Console.WriteLine("6. Mostrar gráfico");
+                Console.WriteLine("7. Existe");
+                Console.WriteLine("8. Eliminar una ocurrencia");
+                Console.WriteLine("9. Eliminar todas las ocurrencias");
+                Console.WriteLine("0. Salir");
+                Console.Write("Ingrese una opción: ");
+                option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.Write("Dato a insertar: ");
+                        list.AddOrdered(Console.ReadLine());
+                        break;
+                    case 2:
+                        list.DisplayForward();
+                        break;
+                    case 3:
+                        list.DisplayBackward();
+                        break;
+                    case 4:
+                        list.SortDescending();
+                        Console.WriteLine("Lista ordenada descendentemente.");
+                        break;
+                    case 5:
+                        list.Moda();
+                        break;
+                    case 6:
+                        list.ShowGraph();
+                        break;
+                    case 7:
+                        Console.Write("Ingrese dato a buscar: ");
+                        Console.WriteLine(list.Exists(Console.ReadLine()) ? "Existe" : "No existe");
+                        break;
+                    case 8:
+                        Console.Write("Dato a eliminar una vez: ");
+                        list.RemoveOne(Console.ReadLine());
+                        break;
+                    case 9:
+                        Console.Write("Dato a eliminar todas las veces: ");
+                        list.RemoveAll(Console.ReadLine());
+                        break;
+                }
+            } while (option != 0);
+        }
+    }
+}
